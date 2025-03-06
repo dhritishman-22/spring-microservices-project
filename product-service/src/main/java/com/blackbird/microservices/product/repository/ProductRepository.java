26ecedcab721c886dd48fc1b7c86d2cd97a87be0
dhritishman <dhritishman.kotaki@getvymo.com>
2025-03-07 02:41:07 +0530

    initial commit

---
+++ product-service/src/main/java/com/blackbird/microservices/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.blackbird.product.repository;
+
+import com.blackbird.product.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
